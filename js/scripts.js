d46097306f35ee875ec8f743840665bcb5fe6d86
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-11-30 23:10:02 +0100

    Add statement to prompt and remove space before

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ var allNames = femaleNames.concat(maleNames);
 
 console.log(allNames);
 
-var newName = prompt ();
+var newName = prompt('Enter name');
 
 if (allNames.indexOf(newName) === -1) {
 	allNames.push(newName);
